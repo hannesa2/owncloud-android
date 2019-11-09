b185c6403edff09449310d76f3ea0c3489d79a44
Hannes Achleitner <hannes.software@gmx.at>
2019-11-09 13:32:21 +0100

    automatic versionname
    adjust versioncode

--- signing/decrypt.sh
+++ signing/decrypt.sh
@@ -2,7 +2,7 @@
 
 pwd
 
-if [ -z "$CRYPT_PASS" ]
+if [[ -z "$CRYPT_PASS" ]]
 then
    echo "\$CRYPT_PASS is empty"
    exit 1
@@ -13,8 +13,8 @@ fi
 cd signing
 
 # to encrypt
-#openssl aes-256-cbc -a -salt -k "CRYPT_PASS" -in GooglePlay.p12 -out GooglePlay.p12.enc
-#openssl aes-256-cbc -a -salt -k "CRYPT_PASS" -in release.keystore -out release.keystore.enc
+#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in GooglePlay.p12 -out GooglePlay.p12.enc
+#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in release.keystore -out release.keystore.enc
 
 # Ubuntu 18.04 (openssl 1.1.0g+) needs -md md5
 # https://askubuntu.com/questions/1067762/unable-to-decrypt-text-files-with-openssl-on-ubuntu-18-04/1076708
