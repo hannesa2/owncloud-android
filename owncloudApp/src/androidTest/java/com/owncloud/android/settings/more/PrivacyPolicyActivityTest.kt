4da7c0cc585d9cac4a7e247def0b773971541870
Hannes Achleitner <hannes.software@gmx.at>
2020-02-16 08:50:21 +0100

    Github CI with action
    Signed-off-by: dependabot[bot] <support@github.com>

--- owncloudApp/src/androidTest/java/com/owncloud/android/settings/more/PrivacyPolicyActivityTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/settings/more/PrivacyPolicyActivityTest.kt
@@ -31,6 +31,7 @@ import androidx.test.espresso.web.webdriver.DriverAtoms.getText
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.owncloud.android.R
+import com.owncloud.android.testutil.annotation.FailsOnGithubAction
 import com.owncloud.android.presentation.ui.settings.PrivacyPolicyActivity
 import com.owncloud.android.utils.matchers.isDisplayed
 import org.hamcrest.Matchers.containsString
@@ -53,6 +54,7 @@ class PrivacyPolicyActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun checkTitle() {
         onView(withText(R.string.actionbar_privacy_policy)).check(matches(isDisplayed()))
     }
@@ -63,6 +65,7 @@ class PrivacyPolicyActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun privacyPolicyLoaded() {
         onWebView().withElement(findElement(Locator.ID, introductionID))
             .check(webMatches(getText(), containsString(introductionText)))
