65b48e607dcdd8c487958af8923b45526175f8e9
Hannes Achleitner <hannes.software@gmx.at>
2020-02-16 08:50:21 +0100

    Github CI with action
    Signed-off-by: dependabot[bot] <support@github.com>

--- owncloudApp/src/androidTest/java/com/owncloud/android/authentication/LoginActivityTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/authentication/LoginActivityTest.kt
@@ -63,6 +63,7 @@ import com.owncloud.android.testutil.OC_AUTH_TOKEN_TYPE
 import com.owncloud.android.testutil.OC_BASIC_PASSWORD
 import com.owncloud.android.testutil.OC_BASIC_USERNAME
 import com.owncloud.android.testutil.OC_SERVER_INFO
+import com.owncloud.android.testutil.annotation.FailsOnGithubAction
 import com.owncloud.android.utils.click
 import com.owncloud.android.utils.matchers.assertVisibility
 import com.owncloud.android.utils.matchers.isDisplayed
@@ -174,6 +175,7 @@ class LoginActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun initialViewStatus_brandedOptions_serverInfoInSetup() {
         launchTest(showServerUrlInput = false, serverUrl = OC_SERVER_INFO.baseUrl)
 
@@ -299,6 +301,7 @@ class LoginActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun checkServerInfo_isSuccess_basicModifyUrlInput() {
         launchTest()
 
@@ -339,6 +342,7 @@ class LoginActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun checkServerInfo_isError_emptyUrl() {
         launchTest()
 
@@ -395,6 +399,7 @@ class LoginActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun loginBasic_callLoginBasic() {
         launchTest()
 
@@ -413,6 +418,7 @@ class LoginActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun loginBasic_callLoginBasic_trimUsername() {
         launchTest()
 
@@ -431,6 +437,7 @@ class LoginActivityTest {
     }
 
     @Test
+    @FailsOnGithubAction
     fun loginBasic_showOrHideFields() {
         launchTest()
 
