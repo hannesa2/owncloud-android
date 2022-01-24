9d30919a855176c1a88955e3ef5a07e2d239d3c9
Hannes Achleitner <hannes.software@gmx.at>
2022-01-24 07:50:02 +0100

    Bump Exoplayer

--- owncloudApp/src/main/java/com/owncloud/android/ui/preview/PreviewVideoFragment.java
+++ owncloudApp/src/main/java/com/owncloud/android/ui/preview/PreviewVideoFragment.java
@@ -428,7 +428,7 @@ public class PreviewVideoFragment extends FileFragment implements View.OnClickLi
 
         // Prepare video player asynchronously
         new PrepareVideoPlayerAsyncTask(getActivity(), this,
-                getFile(), mAccount, mainHandler).execute();
+                getFile(), mAccount).execute();
     }
 
     /**
