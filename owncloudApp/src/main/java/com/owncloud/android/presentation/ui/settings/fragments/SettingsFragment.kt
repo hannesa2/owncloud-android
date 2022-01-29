b9652b240cc604b6a16ebf6015212d2e06808c33
Hannes Achleitner <hannes.software@gmx.at>
2022-01-29 08:54:24 +0100

    Bump Koin 3.2

--- owncloudApp/src/main/java/com/owncloud/android/presentation/ui/settings/fragments/SettingsFragment.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/settings/fragments/SettingsFragment.kt
@@ -75,7 +75,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 true
             }
         } else {
-            settingsScreen?.removePreferenceFromScreen(prefPrivacyPolicy)
+            prefPrivacyPolicy?.let { settingsScreen?.removePreferenceFromScreen(it) }
         }
 
         subsectionWhatsNew?.setOnPreferenceClickListener {
