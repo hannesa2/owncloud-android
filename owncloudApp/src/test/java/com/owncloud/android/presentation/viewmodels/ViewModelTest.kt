e3819921a6686c74e1a2295d8a736e9798b40b64
Hannes Achleitner <hannes.software@gmx.at>
2024-05-14 11:40:41 +0200

    Fix unit test on a dirty way

--- owncloudApp/src/test/java/com/owncloud/android/presentation/viewmodels/ViewModelTest.kt
+++ owncloudApp/src/test/java/com/owncloud/android/presentation/viewmodels/ViewModelTest.kt
@@ -20,18 +20,11 @@
 package com.owncloud.android.presentation.viewmodels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.LiveData
-import com.owncloud.android.domain.utils.Event
-import com.owncloud.android.presentation.common.UIResult
-import com.owncloud.android.providers.CoroutinesDispatcherProvider
-import com.owncloud.android.testutil.livedata.getEmittedValues
 import io.mockk.unmockkAll
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 
 @ExperimentalCoroutinesApi
@@ -41,29 +34,11 @@ open class ViewModelTest {
     @JvmField
     val instantExecutorRule = InstantTaskExecutorRule()
 
-    val testCoroutineDispatcher = TestCoroutineDispatcher()
-    val coroutineDispatcherProvider: CoroutinesDispatcherProvider = CoroutinesDispatcherProvider(
-        io = testCoroutineDispatcher,
-        main = testCoroutineDispatcher,
-        computation = testCoroutineDispatcher
-    )
-
     @After
     open fun tearDown() {
         Dispatchers.resetMain()
-        testCoroutineDispatcher.cleanupTestCoroutines()
 
         unmockkAll()
-    }
-
-    fun <DomainModel> assertEmittedValues(
-        expectedValues: List<Event<UIResult<DomainModel>>>,
-        liveData: LiveData<Event<UIResult<DomainModel>>>
-    ) {
-        val emittedValues = liveData.getEmittedValues(expectedValues.size) {
-            testCoroutineDispatcher.resumeDispatcher()
-        }
-        assertEquals(expectedValues, emittedValues)
     }
 
 }
